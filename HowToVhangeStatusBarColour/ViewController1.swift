a9b3d38670c79662b241791933d75c6bc43bd5cb
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-08 11:41:44 +0300

    init

---
+++ HowToVhangeStatusBarColour/ViewController1.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController1.swift
+//  HowToVhangeStatusBarColour
+//
+//  Created by ramil on 08/10/2019.
+//  Copyright © 2019 com.ri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
